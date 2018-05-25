e62840dd2d08c9ef6577b3c0bfc34f0d600d0e2c
shubham jain <shubhamjain2908@gmail.com>
2018-05-25 16:20:58 +0530

    proof Of Work and genesis block added

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -3,6 +3,10 @@ const sha256 = require('sha256');
 function Blockchain() {
     this.chain = [];
     this.pendingTransactions = [];
+    /**
+     * Creating Genesis block : starting block of blockchain
+     */
+    this.createNewBlock(100, '0', '0');
 }
 
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
@@ -40,5 +44,23 @@ Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, n
     const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
     const hash = sha256(dataAsString);
     return hash;
+}
+
+Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData) {
+/**
+ * bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce)
+ * => repeatedly hash block until it finds out correct hash i.e., 4 0's (0000) at starting
+ * => uses currectly blockdata and previous hash
+ * => continuously changes nonce value uptill it finds correct hash
+ * => returns nonce value when correct hash(0000) is created 
+ */
+    let nonce = 0;
+    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    while(hash.substring(0,4) !== '0000') {
+        nonce++;
+        hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+        console.log(hash);
+    }
+    return nonce;
 }
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,23 +1,24 @@
 const Blockchain = require('./blockchain');
 const bitcoin = new Blockchain();
+console.log(bitcoin);
 
-const previousBlockHash = 'SAKHKJ19082301LKJSAD';
-const currentBlockData = [
-    {   
-        amount: 7829,
-        sender: 'Shubham98128',
-        recipent: 'Jain180913210' 
-    },
-    {   
-        amount: 8121,
-        sender: 'SJ1201398109',
-        recipent: 'Jain180913210' 
-    },
-    {   
-        amount: 3892,
-        sender: 'Aj8120912019',
-        recipent: 'Jain180913210' 
-    }
-];
-const nonce = 100;
-console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce ));
+// const previousBlockHash = 'SAKHKJ19082301LKJSAD';
+// const currentBlockData = [
+//     {   
+//         amount: 7829,
+//         sender: 'Shubham98128',
+//         recipent: 'Jain180913210' 
+//     },
+//     {   
+//         amount: 8121,
+//         sender: 'SJ1201398109',
+//         recipent: 'Jain180913210' 
+//     },
+//     {   
+//         amount: 3892,
+//         sender: 'Aj8120912019',
+//         recipent: 'Jain180913210' 
+//     }
+// ];
+// const nonce = 100;
+// console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce ));
