097cab67408ec458b24d9a88904536be96358abf
shubham jain <shubhamjain2908@gmail.com>
2018-05-25 15:51:49 +0530

    sha256 method added

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,3 +1,5 @@
+const sha256 = require('sha256');
+
 function Blockchain() {
     this.chain = [];
     this.pendingTransactions = [];
@@ -32,5 +34,11 @@ Blockchain.prototype.createNewTransactions = function(amount, sender, recipent) 
     this.pendingTransactions.push(newTranscation);
 
     return this.getLastBlock['index'] + 1;
+}
+
+Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
+    const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    const hash = sha256(dataAsString);
+    return hash;
 }
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,17 +1,23 @@
 const Blockchain = require('./blockchain');
-
 const bitcoin = new Blockchain();
-bitcoin.createNewBlock(8211, '01NHAKK8121JAOWNX', '01810JK8121JKAHAZ');
-// bitcoin.createNewBlock(1239, '01810JK8121JKAHAZ', '618JKASKJ012912JS');
-// bitcoin.createNewBlock(8992, '618JKASKJ012912JS', '127823HKKJHAUSIUS');
-bitcoin.createNewTransactions(7182, 'Shubham98128', 'Jain180913210');
 
-bitcoin.createNewBlock(1239, '01810JK8121JKAHAZ', '618JKASKJ012912JS');
-
-bitcoin.createNewTransactions(1829, 'Shubham98128', 'Jain180913210');
-bitcoin.createNewTransactions(8121, 'SJ1201398109', 'Jain180913210');
-bitcoin.createNewTransactions(3892, 'Aj8120912019', 'Jain180913210');
-
-bitcoin.createNewBlock(8992, '618JKASKJ012912JS', '127823HKKJHAUSIUS');
-
-console.log(bitcoin.chain[2]);
+const previousBlockHash = 'SAKHKJ19082301LKJSAD';
+const currentBlockData = [
+    {   
+        amount: 7829,
+        sender: 'Shubham98128',
+        recipent: 'Jain180913210' 
+    },
+    {   
+        amount: 8121,
+        sender: 'SJ1201398109',
+        recipent: 'Jain180913210' 
+    },
+    {   
+        amount: 3892,
+        sender: 'Aj8120912019',
+        recipent: 'Jain180913210' 
+    }
+];
+const nonce = 100;
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce ));
